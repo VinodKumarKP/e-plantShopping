65f8646ebadb904e5a155cfd076acf9e0469b86c
Vinod Kumar KP <vinod.kunum-purath@capgemini.com>
2026-01-17 13:31:11 -0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# coding-project-template
+# e-plantShopping
+
+### Brief Overview
+e-plantShopping is an e-commerce application designed to provide a seamless experience for plant enthusiasts to browse, select, and purchase a wide variety of indoor and outdoor plants. The platform aims to connect users with nature by offering a curated selection of high-quality plants, gardening tools, and accessories, all accessible through a user-friendly interface.
--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,38 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+      const itemCost = parseFloat(item.cost.substring(1));
+      total += item.quantity * itemCost;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+      handleCheckoutShopping(e)
   };
 
-
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
   const handleIncrement = (item) => {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+      dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+      let itemCost = parseFloat(item.cost.substring(1));
+      return item.quantity * itemCost;
   };
 
   return (
@@ -64,5 +75,3 @@ const CartItem = ({ onContinueShopping }) => {
 };
 
 export default CartItem;
-
-
